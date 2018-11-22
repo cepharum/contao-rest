16e931943f6a0d1ea07b548d5d7188567b7363d3
Alexander Urban <lexa@toxa.de>
2018-11-22 22:50:34 +0100

    #2 (WIP): Collections of subpages can now be queried as list or tree

--- src/Controller/Pages.php
+++ src/Controller/Pages.php
@@ -35,6 +35,7 @@ namespace App\Controller;
 use App\Entity\TlPage;
 use App\Mapper\AutoMapper;
 use Psr\Log\LoggerInterface;
+use Doctrine\Common\Collections\Criteria;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -59,24 +60,160 @@ class Pages extends ApiController {
 	}
 
 	/**
-	 * @Route("/{alias}", methods={"GET", "OPTIONS"}, requirements={"alias"="\w+"})
+	 * @Route("/{idOrAlias}", methods={"GET", "OPTIONS"}, requirements={"idOrAlias"="\w+"})
 	 */
-	public function getPageByAlias( $alias, Request $request ) {
+	public function getPageByAlias( $idOrAlias, Request $request ) {
 
-		return $this->queryDatabaseWithCriterias( [ 'alias', 'eq', $alias ], $request->getMethod(), $this->getDataFromUrl( $request->query, 'list' ), null );
+		return $this->queryDatabaseWithCriterias( [ is_numeric( $idOrAlias ) ? 'id' : 'alias', 'eq', $idOrAlias ], $request->getMethod(), $this->getDataFromUrl( $request->query, 'list' ), null, 1 );
+	}
+
+	protected function collectSubPages_recursive( $repository, $id, $maxDepth, $type, $urlData = null, $depth = 0, $path = [] ) {
+
+		$results = [];
+
+		$criteria = Criteria::create();
+		$criteria->where( $this->compileExpression ( 'id', 'eq', $id ) );
+		if ( is_array( $urlData ) ) {
+			foreach ( $urlData as $name => $value ) {
+				if ( preg_match( '/^\$(?:([^:]+):)?(.+)$/', $value, $match ) ) {
+					$operator = $match[1];
+					$value = $match[2];
+				} else {
+					$operator = 'eq';
+				}
+				$criteria->andWhere( $this->compileExpression( $name, $operator, $value ) );
+			}
+		}
+		$criteria = $this->processCriterion( $criteria );
+		$page = $repository->matching( $criteria )->first();
+
+		if ( $page ) {
+			if ( $type == 'list' || $type == 'tree' ) {
+				$pageData = [ 'id' => $page->getId(), 'alias' => $page->getAlias(), 'published' => $page->getPublished() ];
+			}
+			else {
+				$pageData = $this->mapper->toSerializable( $page, 'list' );
+			}
+			if ( $type == 'list' || $type == 'list-data' ) {
+				$results[] = array_merge( [ '_path' => $path ], $pageData );
+			}
+			else {
+				$results[ 'item' ] = $pageData;
+			}
+		}
+		else if ( $type == 'tree-data' ) {
+			$page = $repository->find( $id );
+			$results[ 'item' ] = [ 'id' => $page->getId(), 'alias' => $page->getAlias(), 'published' => $page->getPublished() ];
+		}
+
+		if ( $depth < $maxDepth ) {
+			$children = $repository->findBy(
+				[ 'pid' => $id ],
+				[ 'sorting' => 'ASC' ]
+			);
+			$childrenData = [];
+			if ( is_array( $children ) ) {
+				foreach ( $children as $child ) {
+					$childrenData[] = $this->collectSubPages_recursive( $repository, $child->getId(), $maxDepth, $type, $urlData, $depth + 1, array_merge( $path, [ $id ] ) );
+				}
+			}
+			if ( $type == 'list' || $type == 'list-data' ) {
+				foreach ( $childrenData as $childData ) {
+					$results = array_merge( $results, $childData );
+				}
+			}
+			else {
+				$results[ 'children' ] = $childrenData;
+			}
+		}
+
+		return $results;
 	}
 
 	/**
-	 * @Route("/{id}/{depth}", methods={"GET", "OPTIONS"}, requirements={"id"="\d+", "depth"="\d+"})
+	 * Method getSubPages()
+	 *
+	 * This function is querying Contao's database for a given page and subordinated pages.
+	 *
+	 * @param string $idOrAlias		ID or alias of the first page to look after.
+	 * @param int $maxDepth			Number of levels below first page to look for subordinated pages
+	 *									e.g. 2, to look for direct children and their direct children
+	 * @param null|string $type		Desired format of the response array, may be one of those:
+	 *									'list' (default value)
+	 *											All found pages will be contained in the response
+	 *											as an one-dimensional list,
+	 *											The pages' hierarchy will be represented at every page
+	 *											with an array-field '_path',
+	 *											e.g. '_path' = [3, 7], if the given page is the child
+	 *												of the page with ID 7 and grandchild of the page with ID 3
+	 *											The response will only include basic data about every page,
+	 *											e.g. [ 'id' => 3, 'alias' => 'home', 'published' => 1 ]
+	 *											Like before but the response will contain
+	 *											all the page's data from the database.
+	 *									'tree'
+	 *											All found pages will be contained in the response
+	 *											as a two-dimensional array, following the pages' hierarchy.
+	 *											The response will only include basic data about every page,
+	 *											e.g. [ 'id' => 3, 'alias' => 'home', 'published' => 1 ]
+	 *									'list-data' or 'tree-data'
+	 *											Like before but the response will also contain
+	 *											all the page's data from the database.
+	 *
+	 * This API-route might also combined with URL-filters like '?published=1' (but not when $type == 'tree').
+	 * That's what happens then:
+	 *			With $type == 'list' and $type == 'list-data',
+	 *				the complete list is filtered and contains only those pages which match the filter.
+	 *			With $type == 'tree-data',
+	 *				all found pages will still be included in the response
+	 *				but only the pages which match the filter will be delivered with full data.
+	 *
+	 * @Route("/{idOrAlias}/{maxDepth}/{type}", methods={"GET", "OPTIONS"}, requirements={"idOrAlias"="\w+", "maxDepth"="\d+"})
 	 */
-	public function getSubPages( $id, $depth ) {
+	public function getSubPages( $idOrAlias, $maxDepth, $type = 'list', Request $request ) {
 
-		if ( $depth < 0 ) {
-			$depth = 0;
+		$repo = $this->getDoctrine()->getRepository( TlPage::class );
+
+		$urlData = $this->getDataFromUrl( $request->query, 'list' );
+		if ( ! is_array( $urlData) || count( $urlData ) == 0 ) {
+			$urlData = null;
 		}
-		if ( $depth > 10 ) {
-			$depth = 10;
+		$maxDepth = max( 0, min( $maxDepth, 20 ) );
+		switch ( $type ) {
+			case 'list':
+			case 'list-data':
+			case 'tree-data':
+				break;
+			case 'tree':
+				if ( $urlData != null ) {
+					return $this->json(
+						[ 'error' => "can't combine response type 'tree' with url-filters, use type 'list' or 'tree-data' instead" ],
+						404,
+						$this->qualifyHeaders( [ 'content-type' => 'application/json' ] )
+					);
+				}
+				break;
+			default:
+				return $this->json(
+					[ 'error' => "unknown response type $type" ],
+					404,
+					$this->qualifyHeaders( [ 'content-type' => 'application/json' ] )
+				);
 		}
 
+		$key = is_numeric( $idOrAlias ) ? 'id' : 'alias';
+		$page = $repo->findOneBy( [ $key => $idOrAlias ] );
+		if ( !$page) {
+			return $this->json(
+				[ 'error' => "can't find page with $key '$idOrAlias'" ],
+				404,
+				$this->qualifyHeaders( [ 'content-type' => 'application/json' ] )
+			);
+		}
+
+		return $this->json(
+			[ 'type' => $type, 'data' => $this->collectSubPages_recursive( $repo, $page->getId(), $maxDepth, $type, $urlData ) ],
+			200,
+			$this->qualifyHeaders( [ 'content-type' => 'application/json' ] )
+		);
 	}
 }
